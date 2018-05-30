671c4351e318bacab9e46f80e3f042cfe4ad166b
Jean-Raphaël Poulin Arguin <arguinjr@gmail.com>
2018-05-30 13:46:54 -0400

    Updates

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/home/calm/Programming/Repos/rorscharg.github.io/src/layouts/index.jsx"
+  import data from "/home/calm/Programming/Repos/rorscharg.github.io/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
